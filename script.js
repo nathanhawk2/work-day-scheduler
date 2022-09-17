354a70744e66d785f5a7b0014fea7f99fe533d44
nathanhawk2 <ntrumpet620@gmail.com>
2022-09-17 16:20:58 -0400

    added local storage getter, modified html page, and began creating timer function for each div and coloring for each time of day

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ $('#currentDay').text(currentDay);
 $('saveBtn').click(save)
 function save(event) {
     event.preventDefault();
-    
+
 }
 
 // clear button
@@ -16,4 +16,28 @@ function clearBtn() {
     localStorage.clear();
     location.reload();
 };
+// current hour function
+$('time').each(function () {
 
+    
+        if (currentHour === timeTime) {
+            $(this).addClass("now");
+        } else if (currentHour < timeTime) {
+            $(this).removeClass("now");
+            $(this).addClass("green");
+        } else if (currentHour > timeTime) {
+            $(this).removeClass("green");
+            $(this).addClass("gray");
+        }
+});
+
+// local storage getter
+  $("#09 .text").val(localStorage.getItem("09"));
+  $("#10 .text").val(localStorage.getItem("10"));
+  $("#11 .text").val(localStorage.getItem("11"));
+  $("#12 .text").val(localStorage.getItem("12"));
+  $("#1 .text").val(localStorage.getItem("1"));
+  $("#2 .text").val(localStorage.getItem("2"));
+  $("#3 .text").val(localStorage.getItem("3"));
+  $("#4 .text").val(localStorage.getItem("4"));
+  $("#5 .text").val(localStorage.getItem("5"));
