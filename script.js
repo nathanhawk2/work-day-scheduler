a0dd52b8d4ab514811ae527d54bd4883ffe9db0f
nathanhawk2 <ntrumpet620@gmail.com>
2022-09-20 10:24:00 -0400

    more progress on timeblock function

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 // current time function at top of screen
-var currentTime = moment();
-var currentDay = currentTime.format('dddd, MMMM Do');
+let currentTime = moment();
+let currentDay = currentTime.format('dddd, MMMM Do');
 $('#currentDay').text(currentDay);
+let currentHour = moment().format("HH");
 
 //  save button 
 $('saveBtn').click(save)
@@ -17,27 +18,29 @@ function clearBtn() {
     location.reload();
 };
 // current hour function
-$('time').each(function () {
-
-    
-        if (currentHour === timeTime) {
-            $(this).addClass("now");
-        } else if (currentHour < timeTime) {
-            $(this).removeClass("now");
-            $(this).addClass("green");
-        } else if (currentHour > timeTime) {
-            $(this).removeClass("green");
-            $(this).addClass("gray");
-        }
+console.log('x')
+$('timeBlock').each(function () {
+    let timeTime = $(this).attr('id');
+    console.log(timeTime);
+    console.log('time');
+    if (currentHour === timeTime) {
+        $(this).addClass("now");
+    } else if (currentHour < timeTime) {
+        $(this).removeClass("now");
+        $(this).addClass("green");
+    } else if (currentHour > timeTime) {
+        $(this).removeClass("green");
+        $(this).addClass("gray");
+    }
 });
 
 // local storage getter
-  $("#09 .text").val(localStorage.getItem("09"));
-  $("#10 .text").val(localStorage.getItem("10"));
-  $("#11 .text").val(localStorage.getItem("11"));
-  $("#12 .text").val(localStorage.getItem("12"));
-  $("#1 .text").val(localStorage.getItem("1"));
-  $("#2 .text").val(localStorage.getItem("2"));
-  $("#3 .text").val(localStorage.getItem("3"));
-  $("#4 .text").val(localStorage.getItem("4"));
-  $("#5 .text").val(localStorage.getItem("5"));
+$("#9 .text").val(localStorage.getItem("9"));
+$("#10 .text").val(localStorage.getItem("10"));
+$("#11 .text").val(localStorage.getItem("11"));
+$("#12 .text").val(localStorage.getItem("12"));
+$("#1 .text").val(localStorage.getItem("1"));
+$("#2 .text").val(localStorage.getItem("2"));
+$("#3 .text").val(localStorage.getItem("3"));
+$("#4 .text").val(localStorage.getItem("4"));
+$("#5 .text").val(localStorage.getItem("5"));
