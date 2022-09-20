c1a2e5142534628f4a4d62d9f9b7cb15f8ab3bd8
nathanhawk2 <ntrumpet620@gmail.com>
2022-09-20 18:53:07 -0400

    fixed timing function, fixed local storage getter, made save button that works, finished project

--- script.js
+++ script.js
@@ -5,24 +5,24 @@ $('#currentDay').text(currentDay);
 let currentHour = moment().format("HH");
 
 //  save button 
-$('saveBtn').click(save)
-function save(event) {
+$('.saveBtn').click(function (event) {
     event.preventDefault();
-
-}
+    let value = $(this).siblings('.timeBlock').val();
+    console.log(value)
+    let time = $(this).parent().attr('id');
+    console.log(time)
+    localStorage.setItem(time, value);
+  });
 
 // clear button
-$('clearBtn').click(clearBtn)
+$('.clearBtn').click(clearBtn)
 function clearBtn() {
     localStorage.clear();
     location.reload();
 };
 // current hour function
-console.log('x')
-$('timeBlock').each(function () {
+$('.timeBlock').each(function () {
     let timeTime = $(this).attr('id');
-    console.log(timeTime);
-    console.log('time');
     if (currentHour === timeTime) {
         $(this).addClass("now");
     } else if (currentHour < timeTime) {
@@ -32,15 +32,15 @@ $('timeBlock').each(function () {
         $(this).removeClass("green");
         $(this).addClass("gray");
     }
-});
+})
 
 // local storage getter
-$("#9 .text").val(localStorage.getItem("9"));
-$("#10 .text").val(localStorage.getItem("10"));
-$("#11 .text").val(localStorage.getItem("11"));
-$("#12 .text").val(localStorage.getItem("12"));
-$("#1 .text").val(localStorage.getItem("1"));
-$("#2 .text").val(localStorage.getItem("2"));
-$("#3 .text").val(localStorage.getItem("3"));
-$("#4 .text").val(localStorage.getItem("4"));
-$("#5 .text").val(localStorage.getItem("5"));
+$('#09 .timeBlock').val(localStorage.getItem('09'));
+$('#10 .timeBlock').val(localStorage.getItem('10'));
+$('#11 .timeBlock').val(localStorage.getItem('11'));
+$('#12 .timeBlock').val(localStorage.getItem('12'));
+$('#13 .timeBlock').val(localStorage.getItem('13'));
+$('#14 .timeBlock').val(localStorage.getItem('14'));
+$('#15 .timeBlock').val(localStorage.getItem('15'));
+$('#16 .timeBlock').val(localStorage.getItem('16'));
+$('#17 .timeBlock').val(localStorage.getItem('17'));
